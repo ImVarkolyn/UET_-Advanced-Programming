4f93bcff008109e73236b4d79ef6e0f7c021b481
ImVarkolyn <phuchpgamer@gmail.com>
2024-03-14 16:39:52 +0700

    Update timeConversion.cpp

--- Week 3/timeConversion.cpp
+++ Week 3/timeConversion.cpp
@@ -10,11 +10,17 @@ using namespace std;
  */
 
 string timeConversion(string s) {
-    int n = s.length();
-    if (s[n-2]=='P')
+    int hour = stoi(s.substr(0,2));
+    if (s.substr(8,2)=="PM")
     {
-
+        if (hour!=12) hour +=12;
     }
+    else
+    {
+        if (hour==12) hour=0;
+    }
+    string militaryTime = (hour < 10 ? "0" : "") + to_string(hour) + s.substr(2, 6);
+    return militaryTime;
 }
 
 int main()
