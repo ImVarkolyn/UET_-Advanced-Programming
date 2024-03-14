ec81f70a07a4c64e1093c88f9072ccac5523f32d
ImVarkolyn <phuchpgamer@gmail.com>
2024-03-14 15:56:44 +0700

    Create timeConversion.cpp

---
+++ Week 3/timeConversion.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string timeConversion(string s) {
+    int n = s.length();
+    if (s[n-2]=='P')
+    {
+
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
