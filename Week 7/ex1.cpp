de317f9afcb73e8130bff93ff6016f11d7a0a82f
ImVarkolyn <phuchpgamer@gmail.com>
2024-05-09 13:20:42 +0700

    Update ex1.cpp

--- Week 7/ex1.cpp
+++ Week 7/ex1.cpp
@@ -1,6 +1,6 @@
 /// Recursive binary search
 
-#include <bits.stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -11,9 +11,9 @@ int binarySearch(const vector<int>& arr, int n, int low, int high) {
         if (arr[mid] == n) {
             return mid;
         } else if (arr[mid] < n) {
-            return binarySearch(arr, target, mid + 1, high);
+            return binarySearch(arr, n, mid + 1, high);
         } else {
-            return binarySearch(arr, target, low, mid - 1);
+            return binarySearch(arr, n, low, mid - 1);
         }
     }
     return -1;
@@ -23,10 +23,10 @@ int main() {
     vector<int> arr = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
     int n = 10;
 
-    int result = binarySearch(arr, target, 0, arr.size() - 1);
+    int result = binarySearch(arr, n, 0, arr.size() - 1);
 
     if (result != -1) {
-        cout << "Found at number index: " << result << endl;
+        cout << "Found at number position: " << result+1 << endl;
     } else {
         cout << "Not found" << endl;
     }
